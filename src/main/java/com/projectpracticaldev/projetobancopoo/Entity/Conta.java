51f1660bfa85aab83e77906a4db347db2554323a
Edmilson Fernandes <jos.edmilson5@gmail.com>
2024-10-30 21:12:07 -0300

    Adicionando interface de Conta

---
+++ src/main/java/com/projectpracticaldev/projetobancopoo/Entity/Conta.java
@@ -0,0 +1,14 @@
+package com.projectpracticaldev.projetobancopoo.Entity;
+
+import java.util.List;
+import java.util.AbstractMap.SimpleEntry;
+
+public interface Conta {
+    public SimpleEntry<Boolean, String> depositar(Double valor);
+    public SimpleEntry<Boolean, String> sacar(Double valor);
+    public Double exibirSaldo();
+    public List<Historico> exibirExtrato();
+    public SimpleEntry<Boolean, String> transferir(Double valor);
+    public SimpleEntry<Boolean, String> bloquearConta();
+    public SimpleEntry<Boolean, String> desbloquearConta();
+}
