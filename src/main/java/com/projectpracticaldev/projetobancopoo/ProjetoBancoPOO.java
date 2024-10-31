bdd81381627c74cce00f71ae93ab6879428fef71
Edmilson Fernandes <jos.edmilson5@gmail.com>
2024-10-31 20:33:49 -0300

    Adicionando teste provisorio de transferencia entre contas

--- src/main/java/com/projectpracticaldev/projetobancopoo/ProjetoBancoPOO.java
+++ src/main/java/com/projectpracticaldev/projetobancopoo/ProjetoBancoPOO.java
@@ -8,6 +8,7 @@ import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
 
 import com.projectpracticaldev.projetobancopoo.Entity.Conta;
+import com.projectpracticaldev.projetobancopoo.Entity.ContaBancaria;
 import com.projectpracticaldev.projetobancopoo.Entity.ContaFactory;
 import com.projectpracticaldev.projetobancopoo.Entity.Historico;
 
@@ -26,7 +27,7 @@ public class ProjetoBancoPOO {
             System.out.println("Deu erro no deposito da conta bancaria: " + depositado1.getValue());
             return;
         }
-        SimpleEntry<Boolean, String> saque1 = cb.sacar(500.0);
+        SimpleEntry<Boolean, String> saque1 = cb.sacar(50.0);
 
         if (saque1.getKey() == false) {
             System.out.println("Deu erro no saque da conta bancaria: " + saque1.getValue());
@@ -49,11 +50,25 @@ public class ProjetoBancoPOO {
             System.out.println("Deu erro no deposito na Conta Especial: " + depositado2.getValue());
             return;
         }
-        SimpleEntry<Boolean, String> saque2 = ce.sacar(1000.0);
+        SimpleEntry<Boolean, String> saque2 = ce.sacar(600.0);
 
         if (saque2.getKey() == false) {
             System.out.println("Deu erro no saque na Conta Especial: " + saque2.getValue());
             return;
+        }
+
+        System.out.println("Saldo da conta bancaria: " + ((ContaBancaria)cb).getSaldo());
+        System.out.println("Saldo da conta especial: " + ((ContaBancaria)ce).getSaldo());
+
+        SimpleEntry<Boolean, String> transferido = cb.transferir(100.0, (ContaBancaria)ce);
+
+        if (transferido.getKey() == false) {
+            System.out.println("Deu erro na transferia para Conta Especial: " + transferido.getValue());
+            return;
+        } else {
+            System.out.println("Saldo da conta bancaria: " + ((ContaBancaria)cb).getSaldo());
+            System.out.println("Saldo da conta especial: " + ((ContaBancaria)ce).getSaldo());
+            System.out.println("Transferencia concluida com sucesso!");
         }
         
         List<Historico> extrato2 = ce.exibirExtrato();
@@ -79,6 +94,20 @@ public class ProjetoBancoPOO {
         if (saque3.getKey() == false) {
             System.out.println("Deu erro no saque na Conta Poupança: " + saque3.getValue());
             return;
+        }
+
+        System.out.println("Saldo da conta bancaria: " + ((ContaBancaria)cb).getSaldo());
+        System.out.println("Saldo da conta especial: " + ((ContaBancaria)cp).getSaldo());
+
+        SimpleEntry<Boolean, String> transferido2 = ce.transferir(100.0, (ContaBancaria)cp);
+
+        if (transferido2.getKey() == false) {
+            System.out.println("Deu erro na transferia para Conta Poupança: " + transferido2.getValue());
+            return;
+        } else {
+            System.out.println("Saldo da conta bancaria: " + ((ContaBancaria)cb).getSaldo());
+            System.out.println("Saldo da conta poupanca: " + ((ContaBancaria)cp).getSaldo());
+            System.out.println("Transferencia concluida com sucesso!");
         }
         
         List<Historico> extrato3 = cp.exibirExtrato();
