b60f61e2be901c518c2872e4e419a81ea7332061
Edmilson Fernandes <jos.edmilson5@gmail.com>
2024-10-30 21:06:46 -0300

    Iniciando Projeto

---
+++ src/main/java/com/projectpracticaldev/projetobancopoo/ProjetoBancoPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.projectpracticaldev.projetobancopoo;
+
+/**
+ *
+ * @author edmilson
+ */
+public class ProjetoBancoPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
