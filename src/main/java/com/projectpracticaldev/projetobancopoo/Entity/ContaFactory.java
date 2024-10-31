ab73c5b3f06d77c71b2276212bc16f99f381b3d3
Edmilson Fernandes <jos.edmilson5@gmail.com>
2024-10-30 23:48:43 -0300

    Removendo o saldo da criação das contas

--- src/main/java/com/projectpracticaldev/projetobancopoo/Entity/ContaFactory.java
+++ src/main/java/com/projectpracticaldev/projetobancopoo/Entity/ContaFactory.java
@@ -6,31 +6,34 @@ import java.util.List;
 
 public class ContaFactory {
 
-    public static Conta criarContaBancaria(Integer numero, Integer agencia, String cliente, Double saldo) {
-        return new ContaBancaria(numero, agencia, cliente, saldo, true, new ArrayList<>());
+    public static Conta criarContaBancaria(Integer numero, Integer agencia, String cliente) {
+        return new ContaBancaria(numero, agencia, cliente, 0.0, true, new ArrayList<>());
     }
 
-    public static Conta carregarContaBancaria(Integer numero, Integer agencia, String cliente, Double saldo, Boolean ativa,
+    public static Conta carregarContaBancaria(Integer numero, Integer agencia, String cliente, Double saldo,
+            Boolean ativa,
             List<Historico> listaHistorico) {
         return new ContaBancaria(numero, agencia, cliente, saldo, ativa, listaHistorico);
     }
 
-    public static Conta criarContaEspecial(Integer numero, Integer agencia, String cliente, Double saldo, Double limite) {
-        return new ContaEspecial(numero, agencia, cliente, saldo, true, new ArrayList<>(), limite);
+    public static Conta criarContaEspecial(Integer numero, Integer agencia, String cliente, Double limite) {
+        return new ContaEspecial(numero, agencia, cliente, 0.0, true, new ArrayList<>(), limite);
     }
 
-    public static Conta carregarContaEspecial(Integer numero, Integer agencia, String cliente, Double saldo, Boolean ativa,
+    public static Conta carregarContaEspecial(Integer numero, Integer agencia, String cliente, Double saldo,
+            Boolean ativa,
             List<Historico> listaHistorico, Double limite) {
         return new ContaEspecial(numero, agencia, cliente, saldo, ativa, listaHistorico, limite);
     }
 
-    public static Conta criarContaPoupanca(Integer numero, Integer agencia, String cliente, Double saldo,
+    public static Conta criarContaPoupanca(Integer numero, Integer agencia, String cliente,
             LocalDateTime dataUltimaMovimentacao, Double taxaJuros) {
-        return new ContaPoupanca(numero, agencia, cliente, saldo, true, new ArrayList<>(), dataUltimaMovimentacao,
+        return new ContaPoupanca(numero, agencia, cliente, 0.0, true, new ArrayList<>(), dataUltimaMovimentacao,
                 taxaJuros);
     }
 
-    public static Conta carregarContaPoupanca(Integer numero, Integer agencia, String cliente, Double saldo, Boolean ativa,
+    public static Conta carregarContaPoupanca(Integer numero, Integer agencia, String cliente, Double saldo,
+            Boolean ativa,
             List<Historico> listaHistorico, LocalDateTime dataUltimaMovimentacao, Double taxaJuros) {
         return new ContaPoupanca(numero, agencia, cliente, saldo, ativa, listaHistorico, dataUltimaMovimentacao,
                 taxaJuros);
