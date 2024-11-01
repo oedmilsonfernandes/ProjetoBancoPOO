14f141d98b7a838e95d5271335775024b46b50cd
Edmilson Fernandes <jos.edmilson5@gmail.com>
2024-11-01 00:33:45 -0300

    Adicionando Testes do metodo transferir de ContaBancaria

--- src/test/java/Entity/ContaBancariaTest.java
+++ src/test/java/Entity/ContaBancariaTest.java
@@ -68,4 +68,45 @@ public class ContaBancariaTest {
         assertEquals("Saque", c.getListaHistorico().get(1).tipoMovimentacao());
         assertEquals(100.0, c.getListaHistorico().get(1).valor());
     }
+
+    @Test
+    public void naoDeveTransferirPorTerValorNegativo(){
+        ContaBancaria c = createConta();
+        ContaBancaria aux = createConta();
+        Resultado<Boolean> transferiu = c.transferir(-100.0, aux);
+
+        assertEquals(false, transferiu.retorno());
+        assertEquals("O valor da transferencia é negativo!", transferiu.erro());
+        assertEquals(true, c.getListaHistorico().isEmpty());
+    }
+
+    @Test
+    public void naoDeveTransferirPorqueNaoTemSaldoSuficiente(){
+        ContaBancaria c = createConta();
+        ContaBancaria aux = createConta();
+        Resultado<Boolean> transferiu = c.transferir(100.0, aux);
+
+        assertEquals(false, transferiu.retorno());
+        assertEquals("A conta não tem saldo para essa transferencia!", transferiu.erro());
+        assertEquals(true, c.getListaHistorico().isEmpty());
+    }
+
+    @Test
+    public void deveTransferir(){
+        ContaBancaria c = createConta();
+        ContaBancaria c2 = createConta();
+        c.depositar(100.0);
+
+        Resultado<Boolean> transferiu = c.transferir(100.0, c2);
+
+        assertEquals(true, transferiu.retorno());
+        assertEquals(false, c.getListaHistorico().isEmpty());
+        assertEquals(true, c.getListaHistorico().get(1).tipoMovimentacao().startsWith("Transferencia para"));
+        assertEquals(100.0, c.getListaHistorico().get(1).valor());
+        assertEquals(0.0, c.getSaldo());
+        assertEquals(false, c2.getListaHistorico().isEmpty());
+        assertEquals(true, c2.getListaHistorico().get(0).tipoMovimentacao().startsWith("Transferencia de"));
+        assertEquals(100.0, c2.getListaHistorico().get(0).valor());
+        assertEquals(100.0, c2.getSaldo());
+    }
 }
